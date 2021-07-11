f37b26758863fd0519800f47aa10c4023b69ea46
Screeeech <52552974+Screeeech@users.noreply.github.com>
2021-07-11 15:21:03 -0500

    added app.py and moved style.css into static/css/ and index.html into templates/

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+# renders index.html
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# updates second text field with processed input from text field 1 on submit button
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    text = request.form["user-input"]
+    # this should be where text becomes rephrased
+    processed_text = text.upper()
+    return render_template('index.html', output=processed_text, input=text)
+
+
+# only for testing purposes, we will need to delete before deployment
+if __name__ == "__main__":
+    app.run()
